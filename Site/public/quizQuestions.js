dd82dd0d543e89fab2a9cceed94f11171f3d0863
LucioGarciaRios <lucio.rios@sptech.school>
2023-06-11 22:42:09 -0300

    Questões do quiz em js

---
+++ Site/public/quizQuestions.js
@@ -0,0 +1,188 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Qual é o nome do serviço de assinatura da Sony que permite o acesso a jogos selecionados por um valor mensal?",
+    answer: "PlayStation Plus",
+    options: [
+        "PlayStation Plus",
+        "PlayStation Network",
+        "PlayStation Now",
+        "PlayStation Store"
+     
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual é o icônico personagem do PlayStation?",
+    answer: "Kratos",
+    options: [
+        "Crash Bandicoot",
+        "Nathan Drake",
+        "Kratos",
+        "Sackboy",
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual é o título da série de jogos de corrida exclusiva para PlayStation?",
+    answer:  "Gran Turismo",    
+    options: [
+        "Need for Speed",
+        "Gran Turismo",
+        "Forza Motorsport",
+        "Project CARS"
+    ]
+  },
+    {
+    numb: 4,
+    question:  "Qual foi o ano de lançamento do PlayStation 2?",
+    answer:  "2001",
+    options: [
+        "2000",
+        "2001",
+        "2002",
+        "2003",
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qual é o nome do popular jogo de ação e aventura exclusivo para playstation, ambientado no Japão feudal?",
+    answer:    "Ghost of Tsushima",
+    options: [
+        "Sekiro: Shadows Die Twice",
+        "Nioh",
+        "Ghost of Tsushima",
+        "Tenchu: Stealth Assassins"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Qual foi o primeiro console PlayStation lançado pela Sony?",
+    answer: "PlayStation 1",
+    options: [
+      "PlayStation 1",
+      "PlayStation 2",
+      "PlayStation 3",
+      "PlayStation 4"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Em que ano o primeiro console PlayStation foi lançado?",
+    answer: "1994",
+    options: [
+      "1994",
+      "1995",
+      "1996",
+      "1997"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Qual é o nome do controle padrão do PlayStation?",
+    answer: "DualShock",
+    options: [
+      " Pro Controller",
+      "Move Controller",
+      "DualShock",
+      "Joy-Con"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Qual foi o primeiro jogo exclusivo de sucesso lançado para o PlayStation?",
+    answer: "Crash Bandicoot",
+    options: [
+      " Final Fantasy VII",
+      "Metal Gear Solid",
+      "Resident Evil",
+      "Crash Bandicoot"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Qual é o nome da primeira versão portátil do console PlayStation lançada pela Sony?",
+    answer: "PlayStation Portable (PSP)",
+    options: [
+      " PlayStation Vita",
+      "PlayStation Portable (PSP)",
+      "PlayStation Mini",
+      "PlayStation Go"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "Qual é o nome da tecnologia de feedback tátil avançado presente no controle DualSense?",
+    answer: "Haptic Feedback",
+    options: [
+      "Haptic Feedback",
+      "Adaptive Triggers",
+      "TouchSense",
+      "DualTouch"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "Qual é o nome do console portátil lançado pela Sony em 2011, sucessor do PSP?",
+    answer: "PlayStation Vita",
+    options: [
+      "PlayStation Portable 2",
+      "PlayStation Vita",
+      "PlayStation Mini",
+      "PlayStation Go"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "Qual é o nome da primeira versão do PlayStation lançada pela Sony com suporte a jogos em alta definição (HD)?",
+    answer: "PlayStation 3",
+    options: [
+      "PlayStation 2",
+      "PlayStation 3",
+      "PlayStation 4",
+      "PlayStation 5"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "Qual é o nome do acessório lançado pela Sony que adiciona funcionalidades de realidade virtual ao PlayStation 4?",
+    answer: "PlayStation VR",
+    options: [
+      "PlayStation VR",
+      "PlayStation Move",
+      "PlayStation Camera",
+      "PlayStation VR Aim Controller"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Qual é o nome do controle de movimento lançado para o PlayStation 3, que utiliza sensores de movimento e permite interações precisas nos jogos?",
+    answer: "PlayStation Move",
+    options: [
+      "DualShock",
+      "DualSense",
+      "PlayStation Move",
+      "PlayStation Motion"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
